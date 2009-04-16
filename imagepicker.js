478f3c7059a3a41d299f2375674c2add6304f8c7
Bob Hutchinson <hutch@midwales.net>
2009-04-16 14:18:12 +0000

    development release version 2 for D5

---
+++ imagepicker.js
@@ -0,0 +1,44 @@
+// $Id$
+// Original Id: upload_progress.js,v 1.1 2007/11/17 06:47:47 pfournier Exp
+/**
+ * @file
+ * Javascript functions for progress bar status on node creation forms
+ *
+ * @author Patrick Fournier <patrick at patrickfournier dot com>
+ * @author Fabio Varesano <fvaresano at yahoo dot it>
+ * Adapted by Bob Hutchinson for imagepicker upload form
+*/
+
+/**
+ * Hide the node form and show the busy div
+*/
+Drupal.imagepicker_upload_progress_hide_timeout = function() {
+  var delay = Drupal.settings["imagepicker_upload_progress"]["delay"];
+  setTimeout('imagepicker_upload_progress_hide()', delay*1000);
+}
+
+function imagepicker_upload_progress_hide() {
+  $('#imagepicker-upload-form').hide();
+  $("#sending").show();
+  $("#imagepicker_upload_progress_cancel_link").click(Drupal.imagepicker_upload_progress_show);
+}
+
+Drupal.imagepicker_upload_progress_show = function() {
+  $('#imagepicker-upload-form').show();
+  $("#sending").hide();
+
+  // "reload" the form
+  window.location = window.location;
+}
+
+/**
+ * Attaches the upload behaviour to the imagepicker upload form.
+ */
+Drupal.imagepicker_upload_progress = function() {
+  $('#imagepicker-upload-form').submit(Drupal.imagepicker_upload_progress_hide_timeout);
+}
+
+// Global killswitch
+if (Drupal.jsEnabled) {
+  $(document).ready(Drupal.imagepicker_upload_progress);
+}
