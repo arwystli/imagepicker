b5b6fe13a811fad02b461570688bfc08cd3dbd75
Bob Hutchinson <hutch@midwales.net>
2009-07-16 11:45:45 +0000

    added link to groups in admin mode, fixed bug in language in template

--- imagepicker-page.tpl.php
+++ imagepicker-page.tpl.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language ?>" xml:lang="<?php print $language ?>">
 <?php
 // $Id$
 // $Name$
@@ -10,6 +8,8 @@
  */
 
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language ?>" xml:lang="<?php print $language->language ?>">
 <head>
   <title><?php echo $head_title ?></title>
   <?php echo $styles ?>
